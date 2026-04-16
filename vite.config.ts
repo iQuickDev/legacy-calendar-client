1cc4ffad17982d39358745530c750009b418453e
PolpOnline <git@t0mmy.anonaddy.com>
2026-04-16 23:37:07 +0200

    Add vue devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-        vue(),
         tailwindcss(),
+        vue(),
+        vueDevTools(),
         VitePWA({
             strategies: 'injectManifest',
             srcDir: 'src',
